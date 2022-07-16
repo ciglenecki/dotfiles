86e1c242a2329b3ee3a6c5e8ee0f87b8a5b1d3a0
Matej Ciglenečki <matej.ciglenecki@gmail.com>
2022-07-16 12:51:10 +0200

    daily 2022-07-16-12-51-10

--- .scripts/backup_dotfiles_github.sh
+++ .scripts/backup_dotfiles_github.sh
@@ -4,11 +4,8 @@
 # Author:       Matej Ciglenečki
 # Description:  pushes dotfiles to github. Anacron calls this script daily
 #################################
-HOME="/home/matej"
-
-notify-send "Pushing dotfiles to Github!";
 
 shopt -s expand_aliases
-source /home/matej/.scripts/alias.sh;
+source /home/matej/.scripts/alias.sh
 
 (cfg add -u && cfg commit -m "daily $(date +%F-%H-%M-%S)" && cfg push) >/home/matej/.log/anacron.log 2>&1
